e38204b6562394dc14c4b6c2b93779bc78fedf6e
Frank de Jong <f.jin@protonmail.com>
2021-01-30 19:04:32 +0100

    Add in-memory implementation of `KVStore` and crate documentation

--- tp201-PNA/kvs/src/lib.rs
+++ tp201-PNA/kvs/src/lib.rs
@@ -1,22 +1,33 @@
-// just leave it empty for now
+#![deny(missing_docs)]
+//! A simple in-memory key-value store.
 
+use std::collections::HashMap;
+
+/// A key value store, backed by a `HashMap<String, String>`.
 #[derive(Clone, Debug, Default)]
-pub struct KvStore;
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
 impl KvStore {
+    /// Constructs a new, empty `KVStore`
     pub fn new() -> KvStore {
         KvStore::default()
     }
 
+    /// Returns a copy of the value corresponding to the supplied key,
+    /// if it is present in the map.
     pub fn get(&self, key: String) -> Option<String> {
-        unimplemented!()
+        self.map.get(&key).map(String::to_owned)
     }
 
+    /// Sets the `val` as the value corresponding to `key` in the store.
     pub fn set(&mut self, key: String, val: String) {
-        unimplemented!()
+        self.map.insert(key, val);
     }
 
+    /// Removes the value corresponding to the supplied key.
     pub fn remove(&mut self, key: String) {
-        unimplemented!()
+        self.map.remove(&key);
     }
 }
